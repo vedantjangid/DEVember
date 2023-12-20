b2fbf87e3a310dbdb6851c6a35cf8330492b0dd0
vedant jangid <vedjangid341@gmail.com>
2023-12-21 02:34:31 +0530

    day1 completed

---
+++ src/app/(days)/day1/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const day1 = () => {
+    return (
+        <View>
+            <Text>day1
+            </Text>
+        </View>
+    )
+}
+
+export default day1
